30ce968473b1ca1316190073eef2cd904dafea3f
Andriy Zarevych <39939977+zarevych@users.noreply.github.com>
2020-04-20 14:25:34 +0300

    Create Test-MsolUserSignin.ps1

---
+++ Public/Test-MsolUserSignin.ps1
@@ -0,0 +1,44 @@
+﻿Function Test-MsolUserSignIn () {
+
+<#
+    .SYNOPSIS
+     Check Office365 User SignIn status
+ 
+    .DESCRIPTION
+     Check Office365 User SignIn status
+
+    .PARAMETER UserPrincipalName
+     Office365 UserPrincipalName
+
+    .NOTES
+     File Name  : Check-MsolUserSignIn.ps1
+     Version    : 1.2004
+     Author     : Andriy Zarevych
+
+    Find me on :
+    * My Blog  :	https://angry-admin.blogspot.com/
+    * LinkedIn :	https://linkedin.com/in/zarevych/
+    * Github   :	https://github.com/zarevych
+#>
+    
+    [CmdLetbinding()]
+
+    param(
+        [Parameter(Mandatory = $true,
+        ValueFromPipeline=$true,
+        ValueFromPipelineByPropertyName=$true,
+        Position=0)]
+        [string]$UserPrincipalName
+    )
+
+    $MsolUser = Get-MsolUser -UserPrincipalName $UserPrincipalName | select BlockCredential
+    #Get-MsolUser -UserPrincipalName $UserPrincipalName | select UserPrincipalName, BlockCredential
+
+    if ($MsolUser.BlockCredential) {
+        return $false
+    }
+    else {
+        return $true
+    }
+
+}
