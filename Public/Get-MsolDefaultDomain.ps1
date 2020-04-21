315ffd15d040f136b1f952a530839245859b0f2b
Andriy Zarevych <39939977+zarevych@users.noreply.github.com>
2020-04-21 12:20:38 +0300

    Create Get-MsolDefaultDomain.ps1

---
+++ Public/Get-MsolDefaultDomain.ps1
@@ -0,0 +1,28 @@
+﻿<#
+    .SYNOPSIS
+     Get Office 365 Default Domain
+ 
+    .DESCRIPTION
+     Get Office 365 Default Domain
+
+    .NOTES
+     File Name  : Get-MsolDefaultDomain.ps1
+     Version    : 1.2004
+     Author     : Andriy Zarevych
+
+    Find me on :
+    * My Blog  :	https://angry-admin.blogspot.com/
+    * LinkedIn :	https://linkedin.com/in/zarevych/
+    * Github   :	https://github.com/zarevych
+#>
+
+
+function Get-MsolDefaultDomain () {
+
+    [OutputType([string])]
+
+    $DefaultDomainName = Get-MsolDomain | where { ($_.IsDefault -eq $true) } | select Name
+
+    return $DefaultDomainName.Name
+}
+
