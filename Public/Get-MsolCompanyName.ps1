26f55494d0c9665158ef7944582a2e861661c72e
Andriy Zarevych <39939977+zarevych@users.noreply.github.com>
2020-04-21 12:20:35 +0300

    Create Get-MsolCompanyName.ps1

---
+++ Public/Get-MsolCompanyName.ps1
@@ -0,0 +1,27 @@
+﻿<#
+    .SYNOPSIS
+     Get Office 365 Company Name
+ 
+    .DESCRIPTION
+     Get Office 365 Company Name
+
+    .NOTES
+     File Name  : Get-MsolCompanyName.ps1
+     Version    : 1.2004
+     Author     : Andriy Zarevych
+
+    Find me on :
+    * My Blog  :	https://angry-admin.blogspot.com/
+    * LinkedIn :	https://linkedin.com/in/zarevych/
+    * Github   :	https://github.com/zarevych
+#>
+
+
+function Get-MsolCompanyName () {
+
+    [OutputType([string])]
+
+    $CompanyName = Get-MsolCompanyInformation
+
+    return $CompanyName.DisplayName
+}
